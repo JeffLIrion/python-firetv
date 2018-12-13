d18dd40c577e3d4b04272760de7a8f423d2b1829
Jeff Irion <jefflirion@users.noreply.github.com>
2018-12-12 19:40:20 -0800

    Add support for using 'pure-python-adb' to send ADB commands

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='firetv',
-    version='1.0.7',
+    version='1.0.8',
     description='Communicate with an Amazon Fire TV device via ADB over a network.',
     url='https://github.com/happyleavesaoc/python-firetv/',
     license='MIT',
     author='happyleaves',
     author_email='happyleaves.tfr@gmail.com',
     packages=['firetv'],
-    install_requires=['pycryptodome', 'rsa', 'adb-homeassistant'],
+    install_requires=['pycryptodome', 'rsa', 'adb-homeassistant', 'pure-python-adb-homeassistant'],
     extras_require={
         'firetv-server': ['Flask>=0.10.1', 'PyYAML>=3.12']
     },
